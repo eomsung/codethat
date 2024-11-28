06f4d45028b563e534aea585257ed1526034e5d2
sungmin <usm0259@kaist.ac.kr>
2024-11-28 11:13:25 +0900

    first

---
+++ src/Main.js
@@ -0,0 +1,32 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import App from "./components/App";
+import HomePage from "./pages/HomePage";
+import CourseListPage from "./pages/CourseListPage";
+import CoursePage from "./pages/CoursePage";
+import WishlistPage from "./pages/WishlistPage";
+import QuestionListPage from "./pages/QuestionListPage";
+import QuestionPage from "./pages/QuestionPage";
+import NotFoundPage from "./pages/NotFoundPage";
+function Main() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<HomePage />} />
+          <Route path="courses">
+            <Route index element={<CourseListPage />} />
+            <Route path=":courseSlug" element={<CoursePage />} />
+          </Route>
+          <Route path="questions">
+            <Route index element={<QuestionListPage />} />
+            <Route path=":id" element={<QuestionPage />} />
+          </Route>
+          <Route path="wishlist" element={<WishlistPage />} />
+        </Route>
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Main;
